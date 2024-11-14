006794e539a00febf22f3acd4006e4a7be6fa2c2
Meysam Arab <120136306+Meysam-Arab@users.noreply.github.com>
2024-11-14 13:03:26 +0330

    Add files via upload

---
+++ Website/attentra/config/constants.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hooman-pc
+ * Date: 11/03/2017
+ * Time: 11:14 PM
+ */
+return [
+    'attendanceModule'=>'1',
+    'missionModule'=>'2',
+    'newEmployeeModule'=>'3',
+    'newCompanyModule'=>'4',
+    'newTrackingModule'=>'5',
+];
---
+++ Website/attentra/config/languages.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Meysam
+ * Date: 11/26/2016
+ * Time: 5:40 PM
+ */
+
+return [
+    'en' => 'English',
+    'pr' => 'فارسی',
+];
